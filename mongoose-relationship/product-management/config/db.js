b0b8a99c4b9a79164e9a7ddafee7a2ba3311d73d
mishrabhi <asmyselfabhishek00@gmail.com>
2024-11-21 13:25:27 +0530

    created server and connected DB

---
+++ mongoose-relationship/product-management/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const mongoURI =
+  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.3.1";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = connect;
