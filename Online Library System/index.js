bfd1b07577ed9f458d2de2513223c28820d02027
mishrabhi <asmyselfabhishek00@gmail.com>
2024-11-24 17:54:18 +0530

    created online library system

--- Online Library System/index.js
+++ Online Library System/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const connect = require("./config/db");
 
+const authorRoutes = require("./routes/authorRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const userRoutes = require("./routes/userRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
 const app = express();
+app.use(bodyParser.json());
 const PORT = 5000;
+
+//Routes
+app.use("/authors", authorRoutes);
+app.use("/books", bookRoutes);
+app.use("/users", userRoutes);
+app.use("transactions", transactionRoutes);
 
 app.listen(PORT, async () => {
   await connect();
