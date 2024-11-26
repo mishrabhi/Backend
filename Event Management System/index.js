80e91498722f00b8ead2acf36e75a9f42e83df81
mishrabhi <asmyselfabhishek00@gmail.com>
2024-11-26 21:32:47 +0530

    imported eventRoutes

--- Event Management System/index.js
+++ Event Management System/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const connect = require("./config/db");
 const app = express();
+const eventRoute = require("./routes/eventRoutes");
 
 const PORT = 3000;
+app.use(express.json());
+
+//Routes
+app.use("/", eventRoute);
 
 app.listen(PORT, async () => {
   connect();
