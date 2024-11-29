3661e89660cacf10b2fdfbe7b9b46d3f592610ee
mishrabhi <asmyselfabhishek00@gmail.com>
2024-11-29 14:15:30 +0530

    added auth routes

--- full stack app - backend/routes/auth.route.js
+++ full stack app - backend/routes/auth.route.js
@@ -9,10 +9,12 @@ const router = express.Router();
 router.post("/signup", async (req, res) => {
   const { username, password, email } = req.body;
   try {
+    //check if user already exists
     const existingUser = await User.find({ username });
     if (existingUser)
       return res.status(400).json({ message: "User already exists" });
 
+    //create new user
     const newUser = new User({ username, password, email });
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
@@ -25,13 +27,14 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
+    //find user
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
+    //match hashedpassword
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid credentials" });
-
+    //generate token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
