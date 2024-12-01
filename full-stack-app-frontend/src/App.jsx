4e4b0529921318f5996c356903bbaaa01cf6a69a
mishrabhi <asmyselfabhishek00@gmail.com>
2024-12-01 19:40:04 +0530

    created nav component

--- full-stack-app-frontend/src/App.jsx
+++ full-stack-app-frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
 export default App;
